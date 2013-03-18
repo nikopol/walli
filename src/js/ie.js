e7ee2f9c60c2a2295de96e412799fb15cb3bc38e
niko <nikomomo@gmail.com>
2013-03-18 08:48:10 +0100

    tidy

--- src/js/ie.js
+++ src/js/ie.js
@@ -1,29 +1,25 @@
 /*
 IE SHIT
-niko 2012
+niko 2012-2013
 */
 
 /* array */
 
 if(![].forEach)
-	Array.prototype.forEach=function(cb){ 
-		for(var i=0; i<this.length; ++i)
-			cb(this[i],i,this)
+	Array.prototype.forEach=function(cb,i,_){ 
+		for(_=this,i=0; i<_.length; cb(_[i],i++,_));
 	};
 
 if(![].filter)
-	Array.prototype.filter=function(test){
-		var f = [];
-		for(var i=0; i<this.length; ++i)
-			if(test(this[i],i))
-				f.push(this[i]);
+	Array.prototype.filter=function(cb,i,_,f){
+		for(_=this,f=[],i=0; i<_.length; ++i)
+			cb(_[i],i) && f.push(_[i]);
 		return f;
 	};
 
 if(![].indexOf)
-	Array.prototype.indexOf=function(w,f){
-		for(var i=f||0; i<this.length; ++i)
-			if(this[i] == w)
-				return i;
+	Array.prototype.indexOf=function(w,f,i,_){
+		for(_=this,i=f||0; i<_.length; ++i)
+			if(_[i] == w) return i;
 		return -1;
 	};
